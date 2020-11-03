36e4a6d43e636e4d1824c33ddd86c93b4a856920
DEBAENE Arnaud <adebaene@acoem.local>
2020-11-03 18:16:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+
+namespace SampleProtobuf1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typeModel = RuntimeTypeModel.Default;
+            typeModel.Add<Int32Value>();
+            //typeModel[typeof(Int32Value)].AddField(1, nameof(Int32Value.Value));
+
+            var msg = new MyMessage { Field1 = new Int32Value { Value = 5 } };
+            var copy = Serializer.DeepClone(msg);
+        }
+    }
+}
